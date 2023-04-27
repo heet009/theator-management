b3131da240478001353a9e1bcaf64d1db4f2593a
heet009 <130938077+heet009@users.noreply.github.com>
2023-04-27 14:03:15 +0000

    added dynamic data and api route

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+movies = [
+  {
+    'id':1,'title':'xyz','img':'static/header.jpg','link':'moviepic.html'
+  },
+  {
+    'id':2,'title':'abc','img':'static/eg.jpg','link':'moviepic.html'
+  },
+  {
+    'id':3,'title':'pqr','img':'static/header.jpg','link':'moviepic.html'
+  }
+]
+
 @app.route("/")
-def hello_world():
-  return render_template('home.html')
+def theator_manage():
+  return render_template('home.html',movie=movies)
+
+@app.route("/api/movies")
+def list_movies():
+  return jsonify(movies)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
+
