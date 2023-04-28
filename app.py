4e84faf8b94f36fbbcd855c6eb06318a880bd60e
heet009 <130938077+heet009@users.noreply.github.com>
2023-04-28 14:37:16 +0000

    editing page and hyperlinks

--- app.py
+++ app.py
@@ -4,6 +4,18 @@ app = Flask(__name__)
 
 movies = [
   {
+    'id':1,'title':'xyz','img':'static/eg1.jpg','link':'moviepic.html'
+  },
+  {
+    'id':2,'title':'abc','img':'static/eg.jpg','link':'moviepic.html'
+  },
+  {
+    'id':3,'title':'pqr','img':'static/eg1.jpg','link':'moviepic.html'
+  }
+]
+
+events = [
+  {
     'id':1,'title':'xyz','img':'static/header.jpg','link':'moviepic.html'
   },
   {
@@ -15,12 +27,20 @@ movies = [
 ]
 
 @app.route("/")
-def theator_manage():
+def home_page():
+  return render_template('home.html',movie=movies)
+  
+@app.route("/home")
+def movies_manage():
   return render_template('home.html',movie=movies)
 
 @app.route("/api/movies")
 def list_movies():
   return jsonify(movies)
+
+@app.route("/events")
+def events_manage():
+  return render_template('events.html')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
