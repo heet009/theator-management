fdc1be498ea97449bf03b30a4ad562e1134454e0
heet009 <130938077+heet009@users.noreply.github.com>
2023-04-29 17:35:59 +0000

    making login page and editing

--- app.py
+++ app.py
@@ -1,47 +1,43 @@
 from flask import Flask, render_template, jsonify
+from database import engine
+from sqlalchemy import text
 
 app = Flask(__name__)
 
-movies = [
-  {
-    'id':1,'title':'xyz','img':'static/eg1.jpg','link':'moviepic.html'
-  },
-  {
-    'id':2,'title':'abc','img':'static/eg.jpg','link':'moviepic.html'
-  },
-  {
-    'id':3,'title':'pqr','img':'static/eg1.jpg','link':'moviepic.html'
-  }
-]
 
-events = [
-  {
-    'id':1,'title':'xyz','img':'static/header.jpg','link':'moviepic.html'
-  },
-  {
-    'id':2,'title':'abc','img':'static/eg.jpg','link':'moviepic.html'
-  },
-  {
-    'id':3,'title':'pqr','img':'static/header.jpg','link':'moviepic.html'
-  }
-]
+movies = []
+
+def load_movies():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from movies"))
+    column_names = result.keys()
+
+    for row in result.all():
+      movies.append(dict(zip(column_names,row)))
+    return movies
+
 
 @app.route("/")
 def home_page():
-  return render_template('home.html',movie=movies)
-  
+  movies = load_movies()
+  return render_template('login.html', movie=movies)
+
+
 @app.route("/home")
 def movies_manage():
-  return render_template('home.html',movie=movies)
+  movies = load_movies()
+  return render_template('home.html', movie=movies)
+
 
 @app.route("/api/movies")
 def list_movies():
   return jsonify(movies)
 
-@app.route("/events")
+
+@app.route("/login")
 def events_manage():
-  return render_template('events.html')
+  return render_template('login.html')
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
-
--- database.py
+++ database.py
@@ -1,9 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy import text
 
-con_str = "mysql+pymysql://root:heet@0.tcp.in.ngrok.io:17171/assignment"
+con_str = "mysql+pymysql://root:heet@0.tcp.in.ngrok.io:13850/assignment"
 engine = create_engine(con_str)
 
-with engine.connect() as conn:
-  result = conn.execute(text("select * from movies"))
-  print(result.all())
+
+    
