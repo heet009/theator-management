3b72086564400cba3df47a111989d1735799dc63
heet009 <130938077+heet009@users.noreply.github.com>
2023-05-03 14:25:32 +0000

    most html done

--- app.py
+++ app.py
@@ -1,42 +1,49 @@
-from flask import Flask, render_template, jsonify
-from database import engine
-from sqlalchemy import text
+from flask import Flask, render_template, jsonify,request
+from database import load_movies, load_movies_id
 
 app = Flask(__name__)
-
-
-movies = []
-
-def load_movies():
-  with engine.connect() as conn:
-    result = conn.execute(text("select * from movies"))
-    column_names = result.keys()
-
-    for row in result.all():
-      movies.append(dict(zip(column_names,row)))
-    return movies
 
 
 @app.route("/")
 def home_page():
   movies = load_movies()
-  return render_template('login.html', movie=movies)
+  return render_template('login.html',movie=movies)
+  
+
+@app.route("/logindata",methods=['post'])
+def login_page():
+  data = request.form
+  return jsonify(data)
+  
+
+#@app.route("/login")
+#def events_manage():
+#  return render_template('login.html')
 
 
-@app.route("/home")
+@app.route("/home",methods=['post'])
 def movies_manage():
+  data = request.form
   movies = load_movies()
-  return render_template('home.html', movie=movies)
+  return render_template('home.html', movie=movies,login=data)
 
 
 @app.route("/api/movies")
 def list_movies():
+  movies = load_movies()
   return jsonify(movies)
 
 
-@app.route("/login")
-def events_manage():
-  return render_template('login.html')
+@app.route("/moviepic/<id>")
+def show_mov(id):
+  mov = load_movies_id(id)
+  
+  return render_template('moviepic.html', mov=mov)
+
+
+@app.route("/booking")
+def book():
+  return render_template('booking.html')
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -1,8 +1,28 @@
 from sqlalchemy import create_engine
 from sqlalchemy import text
 
-con_str = "mysql+pymysql://root:heet@0.tcp.in.ngrok.io:13850/assignment"
+con_str = "mysql+pymysql://root:heet@0.tcp.in.ngrok.io:17248/assignment"
 engine = create_engine(con_str)
 
 
-    
+def load_movies():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from movies"))
+    column_names = result.keys()
+    movies = []
+
+    for row in result.all():
+      movies.append(dict(zip(column_names, row)))
+    return movies
+
+
+def load_movies_id(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from movies where id = :val"),
+                          {"val": id})
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      col = result.keys()
+      return dict(zip(col, rows[0]))
