508d5dbe8fd0f735a0cf3a0b74732aaca3a11a9b
heet009 <130938077+heet009@users.noreply.github.com>
2023-05-05 19:20:55 +0000

    Submission time upload

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, jsonify,request
-from database import load_movies, load_movies_id
+from flask import Flask, render_template, jsonify, request
+from database import load_movies, load_movies_id, add_login_db, load_seats, show_time, seat_select, get_showid
+import json
 
 app = Flask(__name__)
 
@@ -7,43 +8,100 @@ app = Flask(__name__)
 @app.route("/")
 def home_page():
   movies = load_movies()
-  return render_template('login.html',movie=movies)
-  
+  print(movies)
+  return render_template('login.html', mov=movies)
 
-@app.route("/logindata",methods=['post'])
+
+@app.route("/logindata", methods=['post'])
 def login_page():
   data = request.form
   return jsonify(data)
-  
+
 
 #@app.route("/login")
 #def events_manage():
 #  return render_template('login.html')
+email = ""
+name = ""
+id1 = 0  #movie_id
+theator = ""  #room
+time_m = ""  #timing
+seats = ""  #all seats
+show_id = 0  #show id
+no_s = 0  #no of seats
+total = 0
 
 
-@app.route("/home",methods=['post'])
+@app.route("/home", methods=['post'])
 def movies_manage():
   data = request.form
+  global email
+  email = request.form['Email']
+  global name
+  name = request.form['Name']
   movies = load_movies()
-  return render_template('home.html', movie=movies,login=data)
-
-
-@app.route("/api/movies")
-def list_movies():
-  movies = load_movies()
-  return jsonify(movies)
+  return render_template('home.html', movie=movies, login=data)
 
 
 @app.route("/moviepic/<id>")
 def show_mov(id):
   mov = load_movies_id(id)
-  
-  return render_template('moviepic.html', mov=mov)
+  show = show_time(id)
+  global id1
+  id1 = id
+
+  print(show)
+  print(mov)
+  return render_template('moviepic.html', mov=mov, sh=show)
 
 
-@app.route("/booking")
-def book():
-  return render_template('booking.html')
+@app.route("/booking/<th>/<time>")
+def book(th, time):
+  seats = load_seats()
+  global theator
+  theator = th
+  global time_m
+  time_m = time
+  mov = load_movies_id(id1)
+  return render_template('booking.html', seat=seats, thea=th, t=time, mov=mov)
+
+
+@app.route("/final")
+def fi():
+  checkedIDs = request.args.get('checkedIDs')
+  checkedIDs = json.loads(checkedIDs)
+  global time_m
+  print(time_m)
+  global id1
+  print(id1)
+  global theator
+  print(theator)
+  show = get_showid(id1, theator, time_m)
+  global show_id
+  sho_id = [int(value) for value in show.values()]
+  show_id = sho_id[0]
+  print(sho_id[0])
+  global seats
+  seats = ','.join(checkedIDs)
+  global no_s
+  for c in checkedIDs:
+    no_s = no_s + 1
+  print(no_s)
+  global total
+  total = no_s * 35
+  seat_select(checkedIDs, theator)
+  mov = load_movies_id(id1)
+  add_login_db(name, email, show_id, no_s, seats)
+  return render_template('final.html',
+                         seatbook=checkedIDs,
+                         name=name,
+                         email=email,
+                         mov=mov,
+                         theator=theator,
+                         time_m=time_m,
+                         seats=seats,
+                         no_s=no_s,
+                         total=total)
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy import text
 
-con_str = "mysql+pymysql://root:heet@0.tcp.in.ngrok.io:17248/assignment"
+con_str = "mysql+pymysql://root:heet@0.tcp.in.ngrok.io:10584/assignment"
 engine = create_engine(con_str)
 
 
@@ -16,6 +16,17 @@ def load_movies():
     return movies
 
 
+def load_seats():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from theater"))
+    column_names = result.keys()
+    seats = []
+
+    for row in result.all():
+      seats.append(dict(zip(column_names, row)))
+    return seats
+
+
 def load_movies_id(id):
   with engine.connect() as conn:
     result = conn.execute(text("select * from movies where id = :val"),
@@ -26,3 +37,80 @@ def load_movies_id(id):
     else:
       col = result.keys()
       return dict(zip(col, rows[0]))
+
+
+def show_time(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from shows where movie_id = :val"),
+                          {'val': id})
+    column_names = result.keys()
+    show = []
+
+    for row in result.all():
+      show.append(dict(zip(column_names, row)))
+    return show
+
+
+def seat_select(check, thea):
+  with engine.connect() as conn:
+    for ch in check:
+      if thea == 't1':
+        query = text("UPDATE theater SET t1 = 'disabled' WHERE seat = :c1")
+        conn.execute(query, {"c1": ch})
+      if thea == 't2':
+        query = text("UPDATE theater SET t2 = 'disabled' WHERE seat = :c1")
+        conn.execute(query, {"c1": ch})
+      if thea == 't3':
+        query = text("UPDATE theater SET t3 = 'disabled' WHERE seat = :c1")
+        conn.execute(query, {"c1": ch})
+      if thea == 't4':
+        query = text("UPDATE theater SET t4 = 'disabled' WHERE seat = :c1")
+        conn.execute(query, {"c1": ch})
+      if thea == 't5':
+        query = text("UPDATE theater SET t5 = 'disabled' WHERE seat = :c1")
+        conn.execute(query, {"c1": ch})
+      if thea == 't6':
+        query = text("UPDATE theater SET t6 = 'disabled' WHERE seat = :c1")
+        conn.execute(query, {"c1": ch})
+      if thea == 't7':
+        query = text("UPDATE theater SET t7 = 'disabled' WHERE seat = :c1")
+        conn.execute(query, {"c1": ch})
+
+      conn.commit()
+
+
+      #conn.close()
+def get_showid(mov_id, room_t, time):
+  with engine.connect() as conn:
+    result = conn.execute(
+      text(
+        "select show_id from shows where movie_id = :val1 AND room = :val2 AND timing = :val3"
+      ), {
+        'val1': mov_id,
+        'val2': room_t,
+        'val3': time
+      })
+
+    #show = []
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      col = result.keys()
+      return dict(zip(col, rows[0]))
+
+
+def add_login_db(name, email, id1, seat_book, sea):
+  with engine.connect() as conn:
+    conn.execute(
+      text(
+        "insert into customer (name1,email,show_id,Seats_Booked,seats) values(:name,:email,:sh_id,:seats_book,:seat)"
+      ), {
+        'name': name,
+        'email': email,
+        'sh_id': id1,
+        'seats_book': seat_book,
+        'seat': sea
+      })
+    conn.commit()
+    conn.close()
